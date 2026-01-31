b955c629ea885fc7c9aa8c1deca368e560bd713a
Solt21 <kipish-diler@mail.ru>
2026-02-01 01:12:43 +0200

    comit
    new+

--- match-pwa/src/app/App.tsx
+++ match-pwa/src/app/App.tsx
@@ -1,12 +1,15 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import { MatchPage } from "../pages/Match/MatchPage";
+import { LoadingPage } from "../pages/Loading/LoadingPage";
+import { WelcomePage } from "../pages/Welcome/WelcomePage";
+import "./styles/loading.css";
+import "./styles/welcome.css";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/match" replace />} />
-      <Route path="/match" element={<MatchPage />} />
-      <Route path="*" element={<div style={{ padding: 24 }}>404</div>} />
+      <Route path="/" element={<LoadingPage />} />
+      <Route path="/welcome" element={<WelcomePage />} />
+      <Route path="*" element={<Navigate to="/welcome" replace />} />
     </Routes>
   );
 }
